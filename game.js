c21fa5fe17de17165897587933b854208e6d1e9b
stefany <estefyz.n@hotmail.com>
2015-02-03 16:32:12 -0500

    creamos la variable random para que la computadora eliga al azar

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
 /**Juego de piedra, papel o tijera en JS***/
 
-var usuarioElige = prompt("¿Piedra,Papel o Tijera?"); 
+var usuarioElige = prompt("¿Piedra,Papel o Tijera?");
+var computadoraElige = Math.random();//computadora al azar. 
