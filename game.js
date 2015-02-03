61adc82f3fa9453d7a7148294be1535faf25cde8
stefany <estefyz.n@hotmail.com>
2015-02-03 17:03:12 -0500

    Creamos indicaciones de computadorla para indicarle piedra, papel y tijera

--- game.js
+++ game.js
@@ -1,4 +1,13 @@
 /**Juego de piedra, papel o tijera en JS***/
 
 var usuarioElige = prompt("¿Piedra,Papel o Tijera?");
-var computadoraElige = Math.random();//computadora al azar. 
+var computadoraElige = Math.random();//computadora al azar.
+
+//Indicaciones de computadora
+if((computadoraElige >= 0)&&(computadoraElige <= 0.33)){
+    alert("piedra");   
+} else if((computadoraElige >= 0.34)&&(computadoraElige <= 0.66)){
+    alert("papel");   
+}else{
+    alert("tijera");  
+} 
